254d90c4034bb306af47024413461ebd0875f5fe
Jan Minar <rdancer@rdancer.org>
2023-11-20 23:25:17 +0000

    Flesh out the composite image generation

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def generate_images():
     images = [generate_placeholder_image(caption) for caption in captions]
     
     # Placeholder for creating a composite image from generated images
-    final_image = create_composite_placeholder_image(images, captions)
+    final_image = create_composite_image(images, captions)
     
     images_data = [{
         'content_type': 'image/jpeg',
@@ -45,10 +45,30 @@ def generate_placeholder_image(caption):
     draw.text((10, 10), caption, fill='black')
     return image
 
-def create_composite_placeholder_image(images, captions):
-    # This function would create a composite image from generated images
-    # For now, it returns a placeholder composite image
-    final_image = Image.new('RGB', (900, 200), color='white')  # Assuming horizontal layout
+def create_composite_image(images, captions):
+    image_width, image_height = images[0].size
+    gap = 10  # White gap between images
+    border = 3  # Border width around each image
+    caption_height = 40  # Space for the caption text
+
+    # Total dimensions including borders and gaps
+    total_width = (image_width * 3) + (gap * 2) + (border * 6)
+    total_height = image_height + caption_height + (border * 2)
+
+    final_image = Image.new('RGB', (total_width, total_height), 'white')
+    draw = ImageDraw.Draw(final_image)
+
+    # Draw images with borders and captions
+    for i, (image, caption) in enumerate(zip(images, captions)):
+        # Calculate x offset for each image considering previous images, gaps, and borders
+        x_offset = (image_width + gap + border * 2) * i
+        # Paste image onto the final image with an offset for borders
+        final_image.paste(image, (x_offset + border, border))
+        # Draw border around the image
+        draw.rectangle([x_offset, 0, x_offset + image_width + (border * 2), image_height + (border * 2)], outline='black', width=border)
+        # Draw caption below the image
+        draw.text((x_offset + border, image_height + (border * 2)), caption, fill='black', font=ImageFont.load_default())
+
     return final_image
 
 def image_to_base64(image, format):

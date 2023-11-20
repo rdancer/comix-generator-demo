2cfa3d4085f6b736544ff9f5dc5a56fea40066f3
Jan Minar <rdancer@rdancer.org>
2023-11-20 23:10:57 +0000

    fixes: pre-flight fails
    route mismatch between frontend and backend :facepalm:

--- app.py
+++ app.py
@@ -1,48 +1,62 @@
 # app.py (Flask Backend)
 
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image, ImageDraw, ImageFont
 import io
+import base64
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all domains on all routes
+CORS(app)
 
-@app.route('/generate_images', methods=['POST'])
+@app.route('/generate-images', methods=['POST'])
 def generate_images():
-    captions = request.json['captions']
-    images = [generate_placeholder_image() for _ in captions]
-    final_image = create_composite_image(images, captions)
-    img_byte_arr = io.BytesIO()
-    final_image.save(img_byte_arr, format='PNG')
-    img_byte_arr.seek(0)
-    return send_file(img_byte_arr, mimetype='image/png', as_attachment=True, download_name="composite.png")
+    data = request.json
+    captions = data['captions']
+    
+    # Placeholder for DALL-E image generation based on captions
+    images = [generate_placeholder_image(caption) for caption in captions]
+    
+    # Placeholder for creating a composite image from generated images
+    final_image = create_composite_placeholder_image(images, captions)
+    
+    images_data = [{
+        'content_type': 'image/jpeg',
+        'base64': image_to_base64(image, 'JPEG'),
+        'prompt': caption
+    } for caption, image in zip(captions, images)]
+    
+    final_image_data = {
+        'content_type': 'image/png',
+        'base64': image_to_base64(final_image, 'PNG'),
+        'prompt': 'Composite image'
+    }
+    
+    return jsonify({
+        'images': images_data,
+        'finalImage': final_image_data
+    })
 
-def generate_placeholder_image():
+def generate_placeholder_image(caption):
     # This function would interface with DALL-E to generate an image
-    # For now, it returns a placeholder image
-    return Image.new('RGB', (400, 400), color='white')
+    # For now, it returns a placeholder image with the caption
+    image = Image.new('RGB', (400, 400), color='white')
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 10), caption, fill='black')
+    return image
 
-def create_composite_image(images, captions):
-    image_height = max(image.size[1] for image in images)
-    caption_height = 40  # Allocate space for caption text
-    total_height_per_segment = image_height + caption_height
-    total_width = max(image.size[0] for image in images)
-    final_image = Image.new('RGB', (total_width, total_height_per_segment * len(images)), 'white')
-    
-    # Use a truetype font if available
-    # font = ImageFont.truetype("arial.ttf", 32)
-    font = ImageFont.load_default()
-    
-    y_offset = 0
-    for i, image in enumerate(images):
-        final_image.paste(image, (0, y_offset))
-        y_offset += image_height
-        draw = ImageDraw.Draw(final_image)
-        draw.text((10, y_offset), captions[i], fill="black", font=font)
-        y_offset += caption_height  # Move to the next image segment position
-    
+def create_composite_placeholder_image(images, captions):
+    # This function would create a composite image from generated images
+    # For now, it returns a placeholder composite image
+    final_image = Image.new('RGB', (900, 200), color='white')  # Assuming horizontal layout
     return final_image
+
+def image_to_base64(image, format):
+    buffered = io.BytesIO()
+    image.save(buffered, format=format)
+    image_bytes = buffered.getvalue()
+    image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+    return image_base64
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
